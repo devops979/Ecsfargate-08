e11da8450ee3973d6ca267835e291511adf7154b
devops979 <devops979@gmail.com>
2025-06-10 15:21:17 +0530

    Update natgw.tf

--- modules/nat/natgw.tf
+++ modules/nat/natgw.tf
@@ -1,22 +1,15 @@
-
 resource "aws_eip" "nat" {
-  count = 2
   tags = {
-    Name = "${var.vpc_name}-nat-eip-${count.index}"
+    Name = "${var.vpc_name}-nat-eip"
   }
 }
-
-
 
 resource "aws_nat_gateway" "demo-natgw" {
-  count         = length(var.public_subnet_ids)
-  allocation_id = aws_eip.nat[count.index].id
-  subnet_id     = var.public_subnet_ids[count.index]
+  allocation_id = aws_eip.nat.id
+  subnet_id     = var.public_subnet_id
 
   tags = {
-    Name = "${var.vpc_name}-nat-gw-${count.index}"
+    Name = "${var.vpc_name}-nat-gw"
   }
-
 }
-
 
