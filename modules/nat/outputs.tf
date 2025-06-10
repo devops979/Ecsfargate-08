1ad3e78a33aab842010a9b37f6dab1ed6fae57fe
devops979 <devops979@gmail.com>
2025-06-10 15:21:52 +0530

    Update outputs.tf

--- modules/nat/outputs.tf
+++ modules/nat/outputs.tf
@@ -1,6 +1,6 @@
 output "natgw_id" {
-  value = aws_nat_gateway.demo-natgw.*.id
+  value = aws_nat_gateway.demo-natgw.id
 }
 output "natgw_eip" {
-  value = aws_eip.nat.*.id
+  value = aws_eip.nat.id
 }
