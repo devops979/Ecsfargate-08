51f78b14618fdf7484f88b2faa34740c3b73b517
devops979 <devops979@gmail.com>
2025-06-10 19:13:43 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,16 +1,31 @@
 terraform {
-  required_version = ">= 1.10.5"
+  required_version = ">= 1.12.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.87.0"
+      version = "5.99.1"
     }
+  }
+
+  backend "s3" {
+    bucket       = "demo-usecases-bucket-new"
+    key          = "usecase-08/workspace/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true # New approach for state locking
   }
 }
 
+
+
 provider "aws" {
   region = var.aws_region
+
+  default_tags {
+    tags = {
+      Environment = terraform.workspace
+      Project     = "ecs-fargate"
+      ManagedBy   = "Terraform"
+    }
+  }
 }
-
-
-data "aws_availability_zones" "available" {}
